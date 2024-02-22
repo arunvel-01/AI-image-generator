a6aa4d574cc62c3f53c06b6601f0f89a66048b89
Arun Vel <arunvelv9253@gmail.com>
2024-02-22 23:21:38 +0530

    Integration of backend with frontend is done

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const FormField = ({labelName, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe}) => {
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
   return (
     <div>
-      <div className='flex items-center gap-2 mb-2'>
+      <div className="flex items-center gap-2 mb-2">
         <label
-        htmlFor={name}
-        className='block text-sm font-medium text-gray-900'
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
         >
           {labelName}
         </label>
         {isSurpriseMe && (
           <button
-          type='button'
-          onClick={handleSurpriseMe}
-          className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black'
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
           >
             Surprise me
           </button>
         )}
       </div>
-      <input 
+      <input
         type={type}
         id={name}
         name={name}
@@ -28,10 +37,10 @@ const FormField = ({labelName, type, name, placeholder, value, handleChange, isS
         value={value}
         onChange={handleChange}
         required
-        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3"
       />
     </div>
   );
-}
+};
 
 export default FormField;
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -41,11 +41,36 @@ const CreatePost = () => {
         setGeneratingImg(false);
       }
     } else {
-      alert("Please provide a proper prompt");
+      alert("Please enter a prompt");
     }
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (form.prompt && form.photo) {
+      setLoading(true);
+
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(form),
+        });
+
+        await response.json();
+        navigate("/");
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert("Please enter a prompt and generate an imagef");
+    }
+  };
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -124,7 +149,7 @@ const CreatePost = () => {
           </p>
           <button
             type="submit"
-            className="mt-3 text-white bg-[6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
           >
             {loading ? "Sharing..." : "share with the community"}
           </button>
